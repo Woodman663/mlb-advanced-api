7e271eefdd6c578120508c5e4f5369591e883733
Jasper Bosman <jasperbosman@gmail.com>
2019-08-11 21:40:44 +0200

    update decimals

---
+++ db/migrate/20190811193948_change_decimal_columns_in_pitches_redux.rb
@@ -0,0 +1,18 @@
+class ChangeDecimalColumnsInPitchesRedux < ActiveRecord::Migration[5.2]
+  def change
+    change_column :pitches, :velocity, :decimal, :precision => 6, :scale => 2
+    change_column :pitches, :prev_pitch_velo, :decimal, :precision => 6, :scale => 2
+    change_column :pitches, :horizontal_movement, :decimal, :precision => 6, :scale => 2
+    change_column :pitches, :prev_pitch_horizontal_movement, :decimal, :precision => 6, :scale => 2
+    change_column :pitches, :vertical_movement, :decimal, :precision => 6, :scale => 2
+    change_column :pitches, :prev_pitch_vertical_movement, :decimal, :precision => 6, :scale => 2
+    change_column :pitches, :horizontal_location, :decimal, :precision => 6, :scale => 2
+    change_column :pitches, :prev_pitch_horizontal_location, :decimal, :precision => 6, :scale => 2
+    change_column :pitches, :vertical_location, :decimal, :precision => 6, :scale => 2
+    change_column :pitches, :prev_pitch_vertical_location, :decimal, :precision => 6, :scale => 2
+    change_column :pitches, :launch_angle, :decimal, :precision => 6, :scale => 2
+    change_column :pitches, :exit_velo, :decimal, :precision => 6, :scale => 2
+    change_column :pitches, :horizontal_release, :decimal, :precision => 6, :scale => 2
+    change_column :pitches, :vertical_release, :decimal, :precision => 6, :scale => 2
+  end
+end
